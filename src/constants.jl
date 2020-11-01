4dd91b5c616524fa5817f31d7ce4b8c4a01d40d8
asaez <alex.saez.12@gmail.com>
2020-11-01 12:27:35 +0100

    REF: rearrange constants

--- src/constants.jl
+++ src/constants.jl
@@ -1,3 +1,12 @@
+# Constants
+const GD = 32.17  # ft/s^2
+const R0 = 2.377e-3  # Sea level density  (slug/ft²)
+
+# Unit conversions
+const RTOD = 57.29578
+const DEG2RAD = 1 / RTOD
+
+# F16 constants
 const AXX = 9496.0  # slug·ft²
 const AYY = 55814.0  # slug·ft²
 const AZZ = 63100.0  # slug·ft²
@@ -13,7 +22,6 @@ const ZPQ = (AXX - AYY) * AXX + AXZS
 const YPR= AZZ - AXX
 
 const WEIGHT= 20500.0  #
-const GD= 32.17  # ft/s^2
 const MASS= WEIGHT / GD  # lb
 
 const S = 300  # ft^2
@@ -25,8 +33,3 @@ const HX = 160.0  # slug·ft²
 const DE_MAX = 25.0  # deg
 const DA_MAX = 20.0  # deg  #XXX: In Stevens' book says 21.5 deg (Appendix A Section A.4)
 const DR_MAX = 30.0  # deg
-
-const RTOD = 57.29578
-const DEG2RAD = 1 / RTOD
-
-const R0 = 2.377e-3  # Sea level density  (slug/ft²)
