e020b0af319b57f3c617d31de2d60a804f5d00b3
asaez <alex.saez.12@gmail.com>
2020-12-21 21:06:33 +0100

    DOC: minimal documentation for simulate

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,3 +1,15 @@
+"""
+    simulate(tini, tfin, dt, x0, mass, xcg, controls)
+
+Propagate a simulation from tini to tfin with dt time step.
+- tini: initial time (s)
+- tfin: final simulation time (s)
+- dt: time step (s)
+- x0: initial state. Array{13, Number} according to `F16.f`
+- mass: aircraft total mass (lb). Constant for the simulation.
+- xcg: aircraft CG position MAC [0-1]. Constant for the simulation.
+- controls: inputs. Array{4, Input} according to `F16.f`
+"""
 function simulate(tini, tfin, dt, x0, mass, xcg, controls)
 
     t = tini
