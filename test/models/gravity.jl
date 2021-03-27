780378629f0e698010de91fabbabafc6cfca69f2
asaez <alex.saez.12@gmail.com>
2021-03-27 21:24:22 +0100

    TEST: unit testing on gravity models

---
+++ test/models/gravity.jl
@@ -0,0 +1,23 @@
+using FlightMechanicsUtils
+using FlightMechanicsSimulator
+using Test
+
+
+@testset "LHDownGravity" begin
+    # Check constructor with default value
+    grav_1 = LHDownGravity()
+    # Check constructor with custom value
+    grav_2 = LHDownGravity(F16.GD * FT2M)
+
+    # Check return type
+    @test isa(grav_1, LHDownGravity)
+    # Check inheritance
+    @test isa(grav_1, FlightMechanicsSimulator.Gravity)
+
+    # Check getters
+    @test isapprox(get_gravity_accel(grav_1), 9.80665)
+    @test isapprox(get_gravity_accel(grav_2), 9.805416)
+
+    @test isapprox(get_gravity_horizon(grav_1), [0, 0, 9.80665])
+    @test isapprox(get_gravity_horizon(grav_2), [0, 0, 9.805416])
+end
