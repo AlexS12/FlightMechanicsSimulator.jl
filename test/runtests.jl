1f0edd18507ea9c2b5964de101dce17d069985b0
asaez <alex.saez.12@gmail.com>
2021-03-29 17:01:12 +0200

    TEST: rename rk4 to simulation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using SafeTestsets
 
-@safetestset "ADC" begin include("adc.jl") end
-@safetestset "Aerodynamics" begin include("aero.jl") end
-@safetestset "Engine" begin include("engine.jl") end
+@safetestset "F16 ADC" begin include("adc.jl") end
+@safetestset "F16 Aerodynamics" begin include("aero.jl") end
+@safetestset "F16 Engine" begin include("engine.jl") end
 @safetestset "SixDOF" begin include("sixdof.jl") end
-@safetestset "Propagator" begin include("rk4.jl") end
+@safetestset "Simulation" begin include("simulation.jl") end
 @safetestset "Trimmer" begin include("trimmer.jl") end
 @safetestset "Inputs" begin include("models/inputs.jl") end
 @safetestset "Atmosphere" begin include("models/atmosphere.jl") end
