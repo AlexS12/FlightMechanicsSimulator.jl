ffb30525832a133fe6c19f01a6e452d7db5249ac
asaez <alex.saez.12@gmail.com>
2021-03-29 17:03:36 +0200

    TEST: reorder tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using SafeTestsets
 
+@safetestset "Atmosphere" begin include("models/atmosphere.jl") end
+@safetestset "Gravity" begin include("models/gravity.jl") end
+@safetestset "Inputs" begin include("models/inputs.jl") end
 @safetestset "F16 ADC" begin include("adc.jl") end
 @safetestset "F16 Aerodynamics" begin include("aero.jl") end
 @safetestset "F16 Engine" begin include("engine.jl") end
-@safetestset "SixDOF" begin include("sixdof.jl") end
-@safetestset "Simulation" begin include("simulation.jl") end
-@safetestset "Trimmer" begin include("trimmer.jl") end
-@safetestset "Inputs" begin include("models/inputs.jl") end
-@safetestset "Atmosphere" begin include("models/atmosphere.jl") end
-@safetestset "Gravity" begin include("models/gravity.jl") end
+@safetestset "F16 SixDOF" begin include("sixdof.jl") end
+@safetestset "F16 Trimmer" begin include("trimmer.jl") end
+@safetestset "F16 Simulation" begin include("simulation.jl") end
