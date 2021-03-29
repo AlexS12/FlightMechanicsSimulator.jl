3c46bb59fe74a711e21f5df43b8b9998f16f22c2
asaez <alex.saez.12@gmail.com>
2021-03-29 17:31:55 +0200

    TEST: moved f16 tests to its own folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,9 @@ using SafeTestsets
 @safetestset "Atmosphere" begin include("models/atmosphere.jl") end
 @safetestset "Gravity" begin include("models/gravity.jl") end
 @safetestset "Inputs" begin include("models/inputs.jl") end
-@safetestset "F16 ADC" begin include("adc.jl") end
-@safetestset "F16 Aerodynamics" begin include("aero.jl") end
-@safetestset "F16 Engine" begin include("engine.jl") end
-@safetestset "F16 SixDOF" begin include("sixdof.jl") end
-@safetestset "F16 Trimmer" begin include("trimmer.jl") end
-@safetestset "F16 Simulation" begin include("simulation.jl") end
+@safetestset "F16 ADC" begin include("f16/adc.jl") end
+@safetestset "F16 Aerodynamics" begin include("f16/aero.jl") end
+@safetestset "F16 Engine" begin include("f16/engine.jl") end
+@safetestset "F16 SixDOF" begin include("f16/sixdof.jl") end
+@safetestset "F16 Trimmer" begin include("f16/trimmer.jl") end
+@safetestset "F16 Simulation" begin include("f16/simulation.jl") end
