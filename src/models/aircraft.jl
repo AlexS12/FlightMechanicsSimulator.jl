1f5961cda67b94504481e6c11183b4ae9bb6c0cb
asaez <alex.saez.12@gmail.com>
2021-03-29 17:21:44 +0200

    DOC: minimal doc on aircraft

--- src/models/aircraft.jl
+++ src/models/aircraft.jl
@@ -1,8 +1,20 @@
 abstract type Aircraft end
 
-# TODO: DOC
+
 # Mass properties
+"""
+    get_mass(ac::Aircraft)
+
+Get aircraft mass (kg).
+"""
 function get_mass(ac::Aircraft) end
+
+
+"""
+    get_inertia_tensor(ac::Aircraft)
+
+Get aircraft inertia tensor (kg·m²).
+"""
 function get_inertia_tensor(ac::Aircraft) end
 
 
@@ -14,11 +26,31 @@ Get aircraft center of gravity position (units of mean aerodynamic chord).
 function get_xcg_mac(ac::Aircraft) end
 
 # Geometric properties
+"""
+    get_surface(ac:Aircraft)
+
+Get aircraft wing surface (m²).
+"""
 function get_surface(ac::Aircraft) end
+
+
+"""
+    get_chord(ac::Aircraft)
+
+Get aircraft mean aerodynamic chord (m).
+"""
 function get_chord(ac::Aircraft) end
+
+
+"""
+    get_wing_span(ac::Aircraft)
+
+Get aircraft wing span (m).
+"""
 function get_wing_span(ac::Aircraft) end
 
 # Forces and moments
+# TODO: document once headers are standarized. Right now they are too specific to F16Stevens
 function calculate_prop_forces_moments end
 function calculate_prop_gyro_effects end
 function calculate_pdot end
